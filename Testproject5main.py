dbe18ee416cf6775a3781917c953cff9ba3f078d
Shanegriffin1 <79542517+Shanegriffin1@users.noreply.github.com>
2021-05-11 10:41:54 +0100

    Add files via upload

---
+++ Testproject5main.py
@@ -0,0 +1,12 @@
+#
+print("trying it out")
+import pandas as pd
+import numpy as np
+df= pd.read_csv("meat_consumption_worldwide (4).csv")
+print(df)
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Shanegriffin1/Python-project-assignment-test-5.git
+git push -u origin main
